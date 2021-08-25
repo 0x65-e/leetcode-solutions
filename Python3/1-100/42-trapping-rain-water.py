37f8464a8330d787831191ec5871c665e7ad4d68
Matthew Craig <16930669+FacelessHacker@users.noreply.github.com>
2021-08-25 15:26:57 -0700

    #42. Trapping Rain Water (Python3) - 141ms/7.80% 15.7MB/10.58%

--- Python3/1-100/42-trapping-rain-water.py
+++ Python3/1-100/42-trapping-rain-water.py
@@ -1,20 +1,20 @@
-#42. Trapping Rain Water (Python3) - 7548ms/5.02% 16.1MB/5.58%
+#42. Trapping Rain Water (Python3) - 141ms/7.80% 15.7MB/10.58%
 class Solution:
     def trap(self, height: List[int]) -> int:
         # Represent water as existing in pool levels
         # E.g. water can exist at level 0, level 1, level 2, the same as elevations, so long
         # as there is a higher elevation on both sides. A height of 3 bounds pool levels 0, 1, and 2 (but not 3)
-        pools = max(height) * [None]
-        water, prev_h = 0, height[0]
+        pool_walls = [(-1, float("inf"))]
+        water = 0
         for i, h in enumerate(height):
-            if h < prev_h:
-                #for level in range(h, prev_h):
-                #    pools[level] = i
-                pools[h:prev_h] = [i] * (prev_h - h)
-            else:
-                for level in range(prev_h, h):
-                    lwall = pools[level]
-                    if lwall != None:
-                        water += i - lwall
-            prev_h = h
+            last_height = 0 # A shorter wall in front means that height of pool has already been counted
+            # Get all walls that are shorter than this wall
+            while (h >= pool_walls[-1][1]):
+                left_wall, wall_height = pool_walls.pop()
+                water += (i - left_wall) * (wall_height - last_height)
+                last_height = wall_height
+            if len(pool_walls) > 1:
+                left_wall, wall_height = pool_walls[-1]
+                water += (i - left_wall) * (h - last_height)
+            pool_walls.append((i+1, h))
         return water
