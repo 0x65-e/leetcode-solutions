29489e3a3ffad7215d1e9eef96e8d9eae948ad6e
Matthew Craig <16930669+FacelessHacker@users.noreply.github.com>
2021-08-26 11:56:47 -0700

    #331. Verify Preorder Serialization of a Binary Tree (Python3) - 28ms/94.13% 14.2MB/89.52%

--- Python3/301-400/331-verify-preorder-serialization-of-a-binary-tree.py
+++ Python3/301-400/331-verify-preorder-serialization-of-a-binary-tree.py
@@ -1,16 +1,16 @@
-#331. Verify Preorder Serialization of a Binary Tree (Python3) - 95ms/5.08% 14.4MB/15.40%
+#331. Verify Preorder Serialization of a Binary Tree (Python3) - 28ms/94.13% 14.2MB/89.52%
 class Solution:
     def isValidSerialization(self, preorder: str) -> bool:
-        valid_parse, remainder = Solution.parse(preorder.split(","))
-        return valid_parse and len(remainder) == 0
-    
-    @staticmethod
-    def parse(elements):
-        """Checks valid preorder traversal of a tree and returns the remaining elements after parsing"""
-        if len(elements) > 0:
-            if elements[0] == '#':
-                return (True, elements[1:])
-            left_valid, right_subtree = Solution.parse(elements[1:])
-            right_valid, remainder = Solution.parse(right_subtree)
-            return (left_valid and right_valid, remainder)
-        return (False, [])
+        valid_children = [0]
+        for elem in preorder.split(","):
+            if valid_children == [1]:
+                # There are additional elements remaining after the parse completed
+                return False
+            if elem == '#':
+                valid_children[-1] += 1
+                while (valid_children[-1] == 2):
+                    valid_children.pop()
+                    valid_children[-1] += 1 # Won't ever be out of bounds
+            else:
+                valid_children.append(0)
+        return valid_children == [1]
