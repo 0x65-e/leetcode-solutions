eeb8d3921275e9eed55b51ec1a55029358f82586
Matthew Craig <16930669+FacelessHacker@users.noreply.github.com>
2021-12-07 15:19:38 -0800

    #1290. Convert Binary Number in a Linked List to Integer (Python3) - 19ms/99.15% 14.3MB/40.21%

--- Python3/1201-1300/1290-convert-binary-number-in-a-linked-list-to-integer.py
+++ Python3/1201-1300/1290-convert-binary-number-in-a-linked-list-to-integer.py
@@ -1,4 +1,4 @@
-#1290. Convert Binary Number in a Linked List to Integer (Python3) - 36ms/25.39% 14.2MB/40.21%
+#1290. Convert Binary Number in a Linked List to Integer (Python3) - 19ms/99.15% 14.3MB/40.21%
 
 # Definition for singly-linked list.
 # class ListNode:
@@ -9,7 +9,7 @@ class Solution:
     def getDecimalValue(self, head: ListNode) -> int:
         dec = 0
         while head != None:
-            dec = dec * 2 + head.val
+            dec = (dec << 1) + head.val
             head = head.next
         return dec
         
