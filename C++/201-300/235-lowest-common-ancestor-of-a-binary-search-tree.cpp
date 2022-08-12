c772dd3a547927705d50b97ec673e6a958ce9da2
0x65-e <16930669+0x65-e@users.noreply.github.com>
2022-08-11 22:21:05 -0700

    #235. Lowest Common Ancestor of a Binary Search Tree (C++) - 44ms/57.84% 23.3MB/63.86%

---
+++ C++/201-300/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,26 @@
+//#235. Lowest Common Ancestor of a Binary Search Tree (C++) - 44ms/57.84% 23.3MB/63.86%
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while (root != nullptr) {
+            if (p->val > root->val && q->val > root->val) {
+                root = root->right;
+            } else if (p->val < root->val && q->val < root->val) {
+                root = root->left;
+            } else {
+                break;
+            }
+        }
+        return root;
+    }
+};
